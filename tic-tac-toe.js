264dfb91ca76fc8dde18d3a3dbf02efb5ce27c49
nraza1 <114371248+nraza1@users.noreply.github.com>
2023-05-09 17:39:57 -0400

    yuh

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,114 +1,107 @@
+const playerOne = 'X';
+const playerTwo = 'O';
+
+let currentPlayer = playerOne;
+
+let gameType;
+
+let gameOver = false;
+
 let board = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
 
-function drawBoard() {
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+const cells = document.querySelectorAll('.cell');
+
+document.querySelector('#singlePlayer').addEventListener('click', function() {
+  gameType = 'single';
+  document.querySelector('#singlePlayer').classList.add('selected');
+  document.querySelector('#multiplayer').classList.remove('selected');
+});
+
+document.querySelector('#multiplayer').addEventListener('click', function() {
+  gameType = 'multi';
+  document.querySelector('#multiplayer').classList.add('selected');
+  document.querySelector('#singlePlayer').classList.remove('selected');
+});
+
+function handleCellClick(e) {
+  const index = parseInt(e.target.getAttribute('data-index'));
+
+  if (board[index] === '' && !gameOver) {
+    board[index] = currentPlayer;
+    e.target.textContent = currentPlayer;
+    checkForWin();
+    switchPlayers();
+    if (gameType === 'single' && !gameOver) {
+      computerTurn();
+    }
+  }
+}
+
+function checkForWin() {
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+      gameOver = true;
+      const winner = board[a];
+      displayGameOver(`${winner} wins!`);
+    }
+  }
+  if (!board.includes('') && !gameOver) {
+    gameOver = true;
+    displayGameOver('Draw!');
+  }
+}
+
+function computerTurn() {
+  const availableCells = getAvailableCells();
+  const randomIndex = Math.floor(Math.random() * availableCells.length);
+  const randomCell = availableCells[randomIndex];
+  board[randomCell] = playerTwo;
+  cells[randomCell].textContent = playerTwo;
+  checkForWin();
+  switchPlayers();
+}
+
+function getAvailableCells() {
+  const availableCells = [];
   for (let i = 0; i < board.length; i++) {
-    document.getElementById(`cell-${i}`).innerText = board[i];
-  }
-}
-
-function getAvailableMoves() {
-  return board.reduce((moves, cell, index) => {
-    if (cell === '') {
-      moves.push(index);
-    }
-    return moves;
-  }, []);
-}
-
-function checkWin(board, player) {
-  const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  return winningCombos.some(combo =>
-    combo.every(cell => board[cell] === player)
-  );
-}
-
-function getComputerMove() {
-  let move;
-
-  // check if the center cell is available
-  if (board[4] === '') {
-    move = 4;
-  } else {
-    // get a list of all available moves
-    const availableMoves = getAvailableMoves();
-
-    // loop through the available moves and choose one that prevents the user from winning
-    for (let i = 0; i < availableMoves.length; i++) {
-      const testBoard = [...board];
-      testBoard[availableMoves[i]] = 'O';
-      if (!checkWin(testBoard, 'X')) {
-        move = availableMoves[i];
-        break;
-      }
-    }
-
-    // if no move was found to prevent user from winning, choose a random available move
-    if (move === undefined) {
-      move = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+    if (board[i] === '') {
+      availableCells.push(i);
     }
   }
-
-  return move;
+  return availableCells;
 }
 
-function play(cellIndex) {
-  if (board[cellIndex] !== '') {
-    return;
-  }
-
-  board[cellIndex] = currentPlayer;
-  drawBoard();
-
-  if (checkWin(board, currentPlayer)) {
-    alert(`${currentPlayer} wins!`);
-    reset();
-    return;
-  }
-
-  if (getAvailableMoves().length === 0) {
-    alert('Draw!');
-    reset();
-    return;
-  }
-
-  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-
-  if (currentPlayer === 'O') {
-    const computerMove = getComputerMove();
-    board[computerMove] = currentPlayer;
-    drawBoard();
-
-    if (checkWin(board, currentPlayer)) {
-      alert(`${currentPlayer} wins!`);
-      reset();
-      return;
-    }
-
-    if (getAvailableMoves().length === 0) {
-      alert('Draw!');
-      reset();
-      return;
-    }
-
-    currentPlayer = 'X';
-  }
+function switchPlayers() {
+  currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
 }
 
-function reset() {
+function displayGameOver(message) {
+  const gameover = document.querySelector('.gameover');
+  gameover.style.display = 'block';
+  const text = gameover.querySelector('.text');
+  text.textContent = message;
+}
+
+function handleRestartClick() {
   board = ['', '', '', '', '', '', '', '', ''];
-  currentPlayer = 'X';
-  drawBoard();
+  currentPlayer = playerOne;
+  gameOver = false;
+  cells.forEach(cell => cell.textContent = '');
+  document.querySelector('.gameover').style.display = 'none';
 }
 
-drawBoard();
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+
+document.querySelector('.restart').addEventListener('click', handleRestartClick);
